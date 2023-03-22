669922c1e0a7670dd81ea4fd5e50bfccc1425ce8
Antonio Luis Martinez <antonillo_1995@hotmail.com>
2023-03-22 12:48:47 +0100

    gestion de acciones si el usuario esta logado o no

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import { notificationController } from "./notifications/notificationController.js";
 import { adListController } from "./ad-list/adListController.js";
+import { userActionsController } from "./user-actions/userActionsController.js";
 
 const adListElement = document.querySelector('.ads-list');
 const notificationsElement = document.querySelector('.notifications')
+const userActionsElement = document.querySelector('.user-actions')
 
 const showMessage = notificationController(notificationsElement);
-adListController(adListElement);
+adListController(adListElement)
+userActionsController(userActionsElement)
 
 adListElement.addEventListener('newNotification', (event) => {
   console.log('I have received the event.', event.detail.message)
---
+++ user-actions/userActionsController.js
@@ -0,0 +1,19 @@
+import { decodeToken } from "../utils/decodeToken.js"
+import { buildGreeting } from "./userActionsView.js"
+
+export function userActionsController (userActionsElement) {
+  const token = localStorage.getItem('token')
+
+  if (token) {
+    const loginLinkElement = userActionsElement.querySelector('#loginLink')
+    const signupLinkElement = userActionsElement.querySelector('#signupLink')
+    loginLinkElement.remove()
+    signupLinkElement.remove()
+
+    const payload = decodeToken(token)
+    userActionsElement.innerHTML = buildGreeting(payload.username)
+  } else {
+    const createAdLinkElement = userActionsElement.querySelector('#createAdLink')
+    createAdLinkElement.remove()
+  }
+}
